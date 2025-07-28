b4c53fc7690fdcb19701c442159847d7aafd57da
pomonella <39027165+pmnlla@users.noreply.github.com>
2025-07-27 23:04:54 -0400

    Update GenerateGerbers.py - Kicad does not actually have a --format flag for plotting

--- ci/tasks/GenerateGerbers.py
+++ ci/tasks/GenerateGerbers.py
@@ -10,7 +10,7 @@ class UploadToKiCanvasTask(task.Task):
             if file.endswith(("kicad_pcb")):
                 self.logger.info(f'Writing gerbers from: {file}')
                 try:
-                    subprocess.run(f"kicad-cli pcb export gerbers -o sanity_output/gerbers/ --format gerberx2 ../{file}", shell=True) # we can discard the output of this
+                    subprocess.run(f"kicad-cli pcb export gerbers -o sanity_output/gerbers/ ../{file}", shell=True) # we can discard the output of this
                     subprocess.run(f"kicad-cli pcb export drill -o saniity_output/gerbers/ ../{file}", shell=True) # and this
                 except FileNotFoundError:
                     self.logger.error("Kicad executable not found! Failing as fallback.")
